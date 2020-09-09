ec4a48850af54b4075fca48d3f5986a2c80c3930
Jaydon Liu <liujaydon@gmail.com>
2020-09-09 08:49:09 +0800

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,111 @@
+/* Back To Top button */
+
+const toTop = document.querySelector(".to-top"); //selects to-top class
+
+window.addEventListener("scroll", () => {   //listen to scrolling event
+    if (window.pageYOffset > 720) {         //condition: scrolled 100px away from top of page
+        toTop.classList.add("active");      // if yes, add ".active" to class "to-top" 
+    } else {
+        toTop.classList.remove("active");   // if not, remove ".active" from class "to-top"
+    }                                       
+})
+
+//Sticky navigation w/ animations
+
+class StickyNavigation {
+	
+	constructor() {
+		this.currentId = null;
+		this.currentTab = null;
+		this.tabContainerHeight = 70;
+		let self = this;
+		$('.et-hero-tab').click(function() { 
+			self.onTabClick(event, $(this)); 
+		});
+		$(window).scroll(() => { this.onScroll(); });       //activate sticky nav
+		// $(window).resize(() => { this.onResize(); });
+	}
+	
+	onTabClick(event, element) {                    //controls nav bar transitions
+		event.preventDefault();
+		let scrollTop = $(element.attr('href')).offset().top - this.tabContainerHeight + 1;   //controls visual spacing when tab navigates
+		$('html, body').animate({ scrollTop: scrollTop }, 300);
+	}
+	
+	onScroll() {
+		this.checkTabContainerPosition();           //activate sticky nav css
+        // this.findCurrentTabSelector();
+	}
+    
+    checkTabContainerPosition() {                   //nav bar sticky positioning
+		let offset = $('.et-hero-tabs').offset().top + $('.et-hero-tabs').height() - this.tabContainerHeight;
+		if($(window).scrollTop() > offset) {
+			$('.et-hero-tabs-container').addClass('et-hero-tabs-container--top'); //add stick to top positioning
+		} 
+		else {
+			$('.et-hero-tabs-container').removeClass('et-hero-tabs-container--top');
+		}
+	}
+
+	// onResize() {
+	// 	if(this.currentId) {
+	// 		this.setSliderCss();
+	// 	}
+	// }
+	
+
+	
+	// findCurrentTabSelector(element) {               
+	// 	let newCurrentId;
+	// 	let newCurrentTab;
+	// 	let self = this;
+	// 	$('.et-hero-tab').each(function() {
+	// 		let id = $(this).attr('href');
+	// 		let offsetTop = $(id).offset().top - self.tabContainerHeight;
+	// 		let offsetBottom = $(id).offset().top + $(id).height() - self.tabContainerHeight;
+	// 		if($(window).scrollTop() > offsetTop && $(window).scrollTop() < offsetBottom) {
+	// 			newCurrentId = id;
+	// 			newCurrentTab = $(this);
+	// 		}
+	// 	});
+	// 	if(this.currentId != newCurrentId || this.currentId === null) {
+	// 		this.currentId = newCurrentId;
+	// 		this.currentTab = newCurrentTab;
+	// 		this.setSliderCss();
+	// 	}
+	// }
+	
+	// setSliderCss() {
+	// 	let width = 0;
+	// 	let left = 0;
+	// 	if(this.currentTab) {
+	// 		width = this.currentTab.css('width');
+	// 		left = this.currentTab.offset().left;
+	// 	}
+	// 	$('.et-hero-tab-slider').css('width', width);
+	// 	$('.et-hero-tab-slider').css('left', left);
+	// }
+	
+}
+
+new StickyNavigation();
+
+
+// Portfolio Filter
+$(document).ready(function(){        //runs when DOM loaded
+    $(".filter-menu").click(function(){         //runs when filters clicked
+        var selected = $(this).attr("project-category");    //selected filter
+        if (selected == "all"){  
+            $(".filter").show("99");    //show selected category
+        } else {
+            $(".filter").hide("99");      //hide unselected
+            $(".filter").filter("."+selected).show("99");   //show selected
+
+        }
+        $("ul .filter-menu").click(function(){
+            $(this).addClass('active').siblings().removeClass('active');    //activate css for filter menu
+        })
+    })
+})
+
+
